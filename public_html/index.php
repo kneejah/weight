dc80527198d43cc27453c74eeafe48dcd5006e06
Nick Moore <kneejah@gmail.com>
2013-04-27 15:41:12 -0700

    more files

---
+++ public_html/index.php
@@ -0,0 +1,10 @@
+<?php
+
+	// Disable this some day
+	error_reporting(-1);
+	ini_set('error_reporting', -1);
+	ini_set('display_errors', 1);
+
+	define('DEBUG_MODE', 1);
+
+	require_once '../System/Engine/Bootstrap.php';
