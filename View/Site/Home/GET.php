caaeafd85ae6f9d166356c667a5e21111a53e5e7
Nick Moore <kneejah@gmail.com>
2013-04-30 00:47:29 -0700

    changing stuff

--- View/Site/Home/GET.php
+++ View/Site/Home/GET.php
@@ -14,17 +14,19 @@
 			$app->menu_items = Controller_Helper::processMenuItems($app->menu_items, $page, $logged_in);
 
 			$user = null;
+			$formatted_weights = array();
 
 			if ($logged_in)
 			{
-				$id = $policy->getData();
+				$userid = $policy->getData();
 				$user_mapper = new Mapper_User();
-				$user = $user_mapper->getUserById($id);
+				$user = $user_mapper->getUserById($userid);
 			}
 
 			return array(
 				'app'        => $app,
 				'breadcrumb' => 'Home',
+				'error'      => Controller_Helper::getError(),
 				'logged_in'  => $logged_in,
 				'user'       => $user
 			);
