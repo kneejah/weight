b19eeaf4725c52ca13e257658f2f23b292269f57
Nick Moore <kneejah@gmail.com>
2013-05-02 17:04:44 -0700

    adding system config file

---
+++ Config/system.php
@@ -0,0 +1,77 @@
+<?php
+
+	class Config_system extends Abstract_Config
+	{
+
+		public function configs()
+		{
+
+			$iniSettings = parse_ini_file(APP_ROOT . 'Config/system.ini');
+
+			return array(
+				'routes' => array(
+					'Site_Signup' => array(
+						'name'    => 'Site_Signup',
+						'uri'     => '/signup',
+						'type'    => 'get,post',
+						'options' => array('post' => array('noview' => true))
+					),
+					'Site_Home' => array(
+						'name'  => 'Site_Home',
+						'uri'   => '/home',
+						'type'  => 'get'
+					),
+					'Site_Records' => array(
+						'name'  => 'Site_Records',
+						'uri'   => '/records',
+						'type'  => 'get'
+					),
+					'Site_Anchor' => array(
+						'name'    => 'Site_Anchor',
+						'uri'     => '/',
+						'type'    => 'get',
+						'options' => array('noview' => true)
+					),
+					'Site_Login'  => array(
+						'name'    => 'Site_Login',
+						'uri'     => '/login',
+						'type'    => 'get,post',
+						'options' => array('post' => array('noview' => true))
+					),
+					'Site_Logout' => array(
+						'name'    => 'Site_Logout',
+						'uri'     => '/logout',
+						'type'    => 'get',
+						'options' => array('noview' => true)
+					),
+					'Api_Weights' => array(
+						'name'    => 'Api_Weights',
+						'uri'     => '/api/weights',
+						'type'    => 'get',
+						'options' => array('api' => true)
+					),
+					'Api_Weight' => array(
+						'name'    => 'Api_Weight',
+						'uri'     => '/api/weight',
+						'type'    => 'post,put,delete',
+						'options' => array('post' => array('api' => true), 'put' => array('api' => true), 'delete' => array('api' => true))
+					)
+				),
+				'cookie' => new \Slim\Middleware\SessionCookie(
+					array(
+						'expires' => '2 weeks',
+						'path' => '/',
+						'domain' => null,
+						'secure' => false,
+						'httponly' => false,
+						'name' => 'slim_session',
+						'secret' => $iniSettings['cookie_secret'],
+						'cipher' => MCRYPT_RIJNDAEL_256,
+						'cipher_mode' => MCRYPT_MODE_CBC
+					)
+				),
+				'password_hash' => $iniSettings['password_hash']
+			);
+		}
+
+	}
