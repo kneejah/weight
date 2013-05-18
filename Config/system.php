eb409b4b65b4c3b13995340669ecd72a21ba5015
Nick Moore <kneejah@gmail.com>
2013-05-18 14:14:00 -0700

    updating fw stuffs

--- Config/system.php
+++ Config/system.php
@@ -9,87 +9,87 @@
 
 			return array(
 				'routes' => array(
-					'Site_Signup' => array(
+					array(
 						'name'    => 'Site_Signup',
 						'uri'     => '/signup',
 						'type'    => 'get,post',
 						'options' => array('post' => array('noview' => true))
 					),
-					'Site_Home' => array(
-						'name'  => 'Site_Home',
-						'uri'   => '/',
-						'type'  => 'get'
+					array(
+						'name' => 'Site_Home',
+						'uri'  => '/',
+						'type' => 'get'
 					),
-					'Site_Records' => array(
-						'name'  => 'Site_Records',
-						'uri'   => '/records',
-						'type'  => 'get'
+					array(
+						'name' => 'Site_Records',
+						'uri'  => '/records',
+						'type' => 'get'
 					),
-					'Site_About' => array(
-						'name'   => 'Site_About',
-						'uri'    => '/about',
-						'type'   => 'get'
+					array(
+						'name' => 'Site_About',
+						'uri'  => '/about',
+						'type' => 'get'
 					),
-					'Site_Tools' => array(
-						'name'   => 'Site_Tools',
-						'uri'    => '/tools',
-						'type'   => 'get'
+					array(
+						'name' => 'Site_Tools',
+						'uri'  => '/tools',
+						'type' => 'get'
 					),
-					'Site_Tools_Import' => array(
-						'name'          => 'Site_Tools_Import',
-						'uri'           => '/tools/import',
-						'type'          => 'post',
-						'options'       => array('noview' => true)
+					array(
+						'name'    => 'Site_Tools_Import',
+						'uri'     => '/tools/import',
+						'type'    => 'post',
+						'options' => array('noview' => true)
 					),
-					'Site_Tools_Export' => array(
-						'name'          => 'Site_Tools_Export',
-						'uri'           => '/tools/export',
-						'type'          => 'get',
-						'options'       => array('noview' => true)
+					array(
+						'name'    => 'Site_Tools_Export',
+						'uri'     => '/tools/export',
+						'type'    => 'get',
+						'options' => array('noview' => true)
 					),
-					'Site_Settings' => array(
-						'name'      => 'Site_Settings',
-						'uri'       => '/settings',
-						'type'      => 'get,post',
-						'options'   => array('post' => array('noview' => true))
+					array(
+						'name'    => 'Site_Settings',
+						'uri'     => '/settings',
+						'type'    => 'get,post',
+						'options' => array('post' => array('noview' => true))
 					),
-					'Site_Account' => array(
-						'name'     => 'Site_Account',
-						'uri'      => '/account',
-						'type'     => 'get,post',
-						'options'  => array('post' => array('noview' => true))
+					array(
+						'name'    => 'Site_Account',
+						'uri'     => '/account',
+						'type'    => 'get,post',
+						'options' => array('post' => array('noview' => true))
 					),
-					'Site_Login'  => array(
+					array(
 						'name'    => 'Site_Login',
 						'uri'     => '/login',
 						'type'    => 'get,post',
 						'options' => array('post' => array('noview' => true))
 					),
-					'Site_Login_Forgot' => array(
-						'name'          => 'Site_Login_Forgot',
-						'uri'           => '/login/forgot',
-						'type'          => 'get,post',
-						'options'       => array('post' => array('noview' => true))
+					array(
+						'name'    => 'Site_Login_Forgot',
+						'uri'     => '/login/forgot',
+						'type'    => 'get,post',
+						'options' => array('post' => array('noview' => true))
 					),
-					'Site_Logout' => array(
+					array(
 						'name'    => 'Site_Logout',
 						'uri'     => '/logout',
 						'type'    => 'get',
 						'options' => array('noview' => true)
 					),
-					'Api_Weights' => array(
+					array(
 						'name'    => 'Api_Weights',
 						'uri'     => '/api/weights',
 						'type'    => 'get',
 						'options' => array('api' => true)
 					),
-					'Api_UserData' => array(
-						'name'     => 'Api_UserData',
-						'uri'      => '/api/userdata',
-						'type'     => 'get',
-						'options'  => array('api' => true)
+					array(
+						'name'    => 'Api_UserData',
+						'uri'     => '/api/userdata',
+						'type'    => 'get',
+						'options' => array('api' => true)
 					),
-					'Api_Weight' => array(
+					array(
 						'name'    => 'Api_Weight',
 						'uri'     => '/api/weight',
 						'type'    => 'post,put,delete',
