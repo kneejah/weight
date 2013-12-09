e0ebee9aaadba293081484ce88ed8dda63a60323
Nick Moore <kneejah@gmail.com>
2013-12-09 08:52:19 -0800

    fade out messages after a set time

--- public_html/assets/js/common.js
+++ public_html/assets/js/common.js
@@ -3,12 +3,24 @@ var monthNames = ["January", "February", "March", "April", "May", "June", "July"
 
 var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+var fadeDelay = 10000; // 10 seconds
+
 function showErrorMessage(message)
 {
 	$('#success_div').hide();
 	$('#error_div').hide();
 	$('#error_div').html('Error: ' + message);
 	$('#error_div').fadeIn();
+
+        setTimeout(function()
+        {
+                $('#error_div').fadeOut("slow", function()
+                {
+                        $('#error_div').html("&nbsp;");
+                        $('#error_div').show();
+                });
+        }, fadeDelay);
+
 }
 
 function showSuccessMessage(message)
@@ -17,4 +29,13 @@ function showSuccessMessage(message)
 	$('#success_div').hide();
 	$('#success_div').html(message);
 	$('#success_div').fadeIn();
+
+	setTimeout(function()
+        {
+                $('#success_div').fadeOut("slow", function()
+		{
+			$('#success_div').html("&nbsp;");
+			$('#success_div').show();
+		});
+        }, fadeDelay);
 }
