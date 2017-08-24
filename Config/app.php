0a9a39a33875b18a2f7722ac82c838f496b9301c
Nick Moore <kneejah@gmail.com>
2017-08-24 16:56:33 -0700

    start to fix the formatting

--- Config/app.php
+++ Config/app.php
@@ -1,83 +1,83 @@
 <?php
 
-	class Config_app extends Abstract_Config
-	{
+class Config_app extends Abstract_Config
+{
 
-		public function configs()
-		{
-			return array(
-				'name' => 'Trackly',
-				'asset_version' => '2',
-				'menu_items' => array(
-					0 => array('page' => 'home',     'name' => 'Home',     'url' => '/'),
-					1 => array('page' => 'records',  'name' => 'Records',  'url' => '/records',  'logged_in' => true),
-					2 => array('page' => 'settings', 'name' => 'Settings', 'url' => '/settings', 'logged_in' => true),
-					3 => array('page' => 'tools',    'name' => 'Tools',    'url' => '/tools',    'logged_in' => true),
-					4 => array('page' => 'about',    'name' => 'About',    'url' => '/about'),
-					5 => array('page' => 'signup',   'name' => 'Sign up',  'url' => '/signup',   'logged_out' => true),
-					6 => array('page' => 'login',    'name' => 'Log in',   'url' => '/login',    'logged_out' => true),
-					7 => array('page' => 'logout',   'name' => 'Log out',  'url' => '/logout',   'logged_in' => true)
-				),
-				'weight_units'     => 'lbs',
-				'height_units'     => 'in',
-				'default_timezone' => 'America/Los_Angeles',
-				'user_settings' => array(
-					0 => array(
-						'name'         => 'graph_smoothing',
-						'title'        => 'Smooth graph',
-						'description'  => 'Show a smoothed graph on the main page',
-						'type_boolean' => true,
-						'default'      => 0,
-						'validate'     => 'boolean',
-						'order'        => 5
-					),
-					1 => array(
-						'name'         => 'height',
-						'title'        => 'Height',
-						'description'  => 'Your height in inches',
-						'type_string'  => true,
-						'default'      => 0,
-						'validate'     => 'height',
-						'order'        => 1
-					),
-					2 => array(
-						'name'         => 'timezone',
-						'title'        => 'Time zone',
-						'description'  => 'Time zone to display, view your choices <a target="_blank" href="http://php.net/manual/en/timezones.php">here</a>',
-						'type_string'  => true,
-						'default'      => 'America/Los_Angeles',
-						'validate'     => 'timezone',
-						'order'        => 6
-					),
-					3 => array(
-						'name'         => 'trend_line',
-						'title'        => 'Trend line',
-						'description'  => 'Show your trend line on the graph as well',
-						'type_boolean' => true,
-						'default'      => 0,
-						'validate'     => 'boolean',
-						'order'        => 4
-					),
-					4 => array(
-						'name'         => 'target_weight',
-						'title'        => 'Target weight',
-						'description'  => 'Target weight you are trying to achieve',
-						'type_string' => true,
-						'default'      => 0,
-						'validate'     => 'weight',
-						'order'        => 2
-					),
-					5 => array(
-						'name'         => 'show_target_weight',
-						'title'        => 'Show target weight',
-						'description'  => 'Show your target weight on the graph',
-						'type_boolean' => true,
-						'default'      => 0,
-						'validate'     => 'boolean',
-						'order'        => 3
-					)
-				)
-			);
-		}
+    public function configs()
+    {
+        return array(
+            'name' => 'Trackly',
+            'asset_version' => '2',
+            'menu_items' => array(
+                0 => array('page' => 'home',     'name' => 'Home',     'url' => '/'),
+                1 => array('page' => 'records',  'name' => 'Records',  'url' => '/records',  'logged_in' => true),
+                2 => array('page' => 'settings', 'name' => 'Settings', 'url' => '/settings', 'logged_in' => true),
+                3 => array('page' => 'tools',    'name' => 'Tools',    'url' => '/tools',    'logged_in' => true),
+                4 => array('page' => 'about',    'name' => 'About',    'url' => '/about'),
+                5 => array('page' => 'signup',   'name' => 'Sign up',  'url' => '/signup',   'logged_out' => true),
+                6 => array('page' => 'login',    'name' => 'Log in',   'url' => '/login',    'logged_out' => true),
+                7 => array('page' => 'logout',   'name' => 'Log out',  'url' => '/logout',   'logged_in' => true)
+            ),
+            'weight_units'     => 'lbs',
+            'height_units'     => 'in',
+            'default_timezone' => 'America/Los_Angeles',
+            'user_settings' => array(
+                0 => array(
+                    'name'         => 'graph_smoothing',
+                    'title'        => 'Smooth graph',
+                    'description'  => 'Show a smoothed graph on the main page',
+                    'type_boolean' => true,
+                    'default'      => 0,
+                    'validate'     => 'boolean',
+                    'order'        => 5
+                ),
+                1 => array(
+                    'name'         => 'height',
+                    'title'        => 'Height',
+                    'description'  => 'Your height in inches',
+                    'type_string'  => true,
+                    'default'      => 0,
+                    'validate'     => 'height',
+                    'order'        => 1
+                ),
+                /*2 => array(
+                    'name'         => 'timezone',
+                    'title'        => 'Time zone',
+                    'description'  => 'Time zone to display, view your choices <a target="_blank" href="http://php.net/manual/en/timezones.php">here</a>',
+                    'type_string'  => true,
+                    'default'      => 'America/Los_Angeles',
+                    'validate'     => 'timezone',
+                    'order'        => 6
+                ),*/
+                3 => array(
+                    'name'         => 'trend_line',
+                    'title'        => 'Trend line',
+                    'description'  => 'Show your trend line on the graph as well',
+                    'type_boolean' => true,
+                    'default'      => 0,
+                    'validate'     => 'boolean',
+                    'order'        => 4
+                ),
+                4 => array(
+                    'name'         => 'target_weight',
+                    'title'        => 'Target weight',
+                    'description'  => 'Target weight you are trying to achieve',
+                    'type_string'  => true,
+                    'default'      => 0,
+                    'validate'     => 'weight',
+                    'order'        => 2
+                ),
+                5 => array(
+                    'name'         => 'show_target_weight',
+                    'title'        => 'Show target weight',
+                    'description'  => 'Show your target weight on the graph',
+                    'type_boolean' => true,
+                    'default'      => 0,
+                    'validate'     => 'boolean',
+                    'order'        => 3
+                )
+            )
+        );
+    }
 
-	}
+}
--- Config/system.php
+++ Config/system.php
@@ -1,123 +1,123 @@
 <?php
 
-	class Config_system extends Abstract_Config
-	{
+class Config_system extends Abstract_Config
+{
 
-		public function configs()
-		{
-			$iniSettings = parse_ini_file(APP_ROOT . 'Config/system.ini');
+    public function configs()
+    {
+        $iniSettings = parse_ini_file(APP_ROOT . 'Config/system.ini');
 
-			return array(
-				'routes' => array(
-					array(
-						'name'    => 'Site_Signup',
-						'uri'     => '/signup',
-						'type'    => 'get,post',
-						'options' => array('post' => array('noview' => true))
-					),
-					array(
-						'name' => 'Site_Home',
-						'uri'  => '/',
-						'type' => 'get'
-					),
-					array(
-						'name' => 'Site_Records',
-						'uri'  => '/records',
-						'type' => 'get'
-					),
-					array(
-						'name' => 'Site_About',
-						'uri'  => '/about',
-						'type' => 'get'
-					),
-					array(
-						'name' => 'Site_Tools',
-						'uri'  => '/tools',
-						'type' => 'get'
-					),
-					array(
-						'name'    => 'Site_Tools_Import',
-						'uri'     => '/tools/import',
-						'type'    => 'post',
-						'options' => array('noview' => true)
-					),
-					array(
-						'name'    => 'Site_Tools_Export',
-						'uri'     => '/tools/export',
-						'type'    => 'get',
-						'options' => array('noview' => true)
-					),
-					array(
-						'name'    => 'Site_Settings',
-						'uri'     => '/settings',
-						'type'    => 'get,post',
-						'options' => array('post' => array('noview' => true))
-					),
-					array(
-						'name'    => 'Site_Account',
-						'uri'     => '/account',
-						'type'    => 'get,post',
-						'options' => array('post' => array('noview' => true))
-					),
-					array(
-						'name'    => 'Site_Account_Cancel',
-						'uri'     => '/account/cancel',
-						'type'    => 'post',
-						'options' => array('noview' => true)
-					),
-					array(
-						'name'    => 'Site_Login',
-						'uri'     => '/login',
-						'type'    => 'get,post',
-						'options' => array('post' => array('noview' => true))
-					),
-					array(
-						'name'    => 'Site_Login_Forgot',
-						'uri'     => '/login/forgot',
-						'type'    => 'get,post',
-						'options' => array('post' => array('noview' => true))
-					),
-					array(
-						'name'    => 'Site_Logout',
-						'uri'     => '/logout',
-						'type'    => 'get',
-						'options' => array('noview' => true)
-					),
-					array(
-						'name'    => 'Api_Weights',
-						'uri'     => '/api/weights',
-						'type'    => 'get',
-						'options' => array('api' => true)
-					),
-					array(
-						'name'    => 'Api_UserData',
-						'uri'     => '/api/userdata',
-						'type'    => 'get',
-						'options' => array('api' => true)
-					),
-					array(
-						'name'    => 'Api_Weight',
-						'uri'     => '/api/weight',
-						'type'    => 'post,put,delete',
-						'options' => array('post' => array('api' => true), 'put' => array('api' => true), 'delete' => array('api' => true))
-					)
-				),
-				'cookie' => new \Slim\Middleware\SessionCookie(
-					array(
-						'expires' => '2 weeks',
-						'path' => '/',
-						'domain' => null,
-						'secure' => false,
-						'httponly' => false,
-						'name' => 'slim_session',
-						'secret' => $iniSettings['cookie_secret'],
-						'cipher' => MCRYPT_RIJNDAEL_256,
-						'cipher_mode' => MCRYPT_MODE_CBC
-					)
-				),
-				'password_hash' => $iniSettings['password_hash'],
-				'sqlite_path'   => $iniSettings['sqlite_path']
-			);
-		}
+        return array(
+            'routes' => array(
+                array(
+                    'name'    => 'Site_Signup',
+                    'uri'     => '/signup',
+                    'type'    => 'get,post',
+                    'options' => array('post' => array('noview' => true))
+                ),
+                array(
+                    'name' => 'Site_Home',
+                    'uri'  => '/',
+                    'type' => 'get'
+                ),
+                array(
+                    'name' => 'Site_Records',
+                    'uri'  => '/records',
+                    'type' => 'get'
+                ),
+                array(
+                    'name' => 'Site_About',
+                    'uri'  => '/about',
+                    'type' => 'get'
+                ),
+                array(
+                    'name' => 'Site_Tools',
+                    'uri'  => '/tools',
+                    'type' => 'get'
+                ),
+                array(
+                    'name'    => 'Site_Tools_Import',
+                    'uri'     => '/tools/import',
+                    'type'    => 'post',
+                    'options' => array('noview' => true)
+                ),
+                array(
+                    'name'    => 'Site_Tools_Export',
+                    'uri'     => '/tools/export',
+                    'type'    => 'get',
+                    'options' => array('noview' => true)
+                ),
+                array(
+                    'name'    => 'Site_Settings',
+                    'uri'     => '/settings',
+                    'type'    => 'get,post',
+                    'options' => array('post' => array('noview' => true))
+                ),
+                array(
+                    'name'    => 'Site_Account',
+                    'uri'     => '/account',
+                    'type'    => 'get,post',
+                    'options' => array('post' => array('noview' => true))
+                ),
+                array(
+                    'name'    => 'Site_Account_Cancel',
+                    'uri'     => '/account/cancel',
+                    'type'    => 'post',
+                    'options' => array('noview' => true)
+                ),
+                array(
+                    'name'    => 'Site_Login',
+                    'uri'     => '/login',
+                    'type'    => 'get,post',
+                    'options' => array('post' => array('noview' => true))
+                ),
+                array(
+                    'name'    => 'Site_Login_Forgot',
+                    'uri'     => '/login/forgot',
+                    'type'    => 'get,post',
+                    'options' => array('post' => array('noview' => true))
+                ),
+                array(
+                    'name'    => 'Site_Logout',
+                    'uri'     => '/logout',
+                    'type'    => 'get',
+                    'options' => array('noview' => true)
+                ),
+                array(
+                    'name'    => 'Api_Weights',
+                    'uri'     => '/api/weights',
+                    'type'    => 'get',
+                    'options' => array('api' => true)
+                ),
+                array(
+                    'name'    => 'Api_UserData',
+                    'uri'     => '/api/userdata',
+                    'type'    => 'get',
+                    'options' => array('api' => true)
+                ),
+                array(
+                    'name'    => 'Api_Weight',
+                    'uri'     => '/api/weight',
+                    'type'    => 'post,put,delete',
+                    'options' => array('post' => array('api' => true), 'put' => array('api' => true), 'delete' => array('api' => true))
+                )
+            ),
+            'cookie' => new \Slim\Middleware\SessionCookie(
+                array(
+                    'expires' => '2 weeks',
+                    'path' => '/',
+                    'domain' => null,
+                    'secure' => false,
+                    'httponly' => false,
+                    'name' => 'slim_session',
+                    'secret' => $iniSettings['cookie_secret'],
+                    'cipher' => MCRYPT_RIJNDAEL_256,
+                    'cipher_mode' => MCRYPT_MODE_CBC
+                )
+            ),
+            'password_hash' => $iniSettings['password_hash'],
+            'sqlite_path'   => $iniSettings['sqlite_path']
+        );
+    }
 
-	}
+}
