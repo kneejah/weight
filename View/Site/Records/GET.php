68bf9e2b3dd3d39a961704dff81ed530f004a853
Nick Moore <kneejah@gmail.com>
2017-08-24 13:59:35 -0700

    fix time zone issues

--- View/Site/Records/GET.php
+++ View/Site/Records/GET.php
@@ -58,7 +58,7 @@
 
 			foreach ($weights as &$weight)
 			{
-				$weight['time'] = date('D F j, Y, g:i a', $weight['create_time']);
+				$weight['time'] = $weight['create_time'] * 1000; // date('D F j, Y, g:i a', $weight['create_time']);
 				$wVal = $weight['weight'];
 				if (round($wVal) == $wVal)
 				{
